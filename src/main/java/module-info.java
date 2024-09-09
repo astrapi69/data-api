f518df00dffe66d66226ad562c0b8481e172e5f0
Asterios Raptis <asteri.raptis@gmail.com>
2024-09-09 16:31:21 +0200

    rename of module to new name 'io.github.astrapisixtynine.data.api'
    - update gradle version to 8.10.1
    - update of gradle-plugin dependency spotless-plugin-gradle to new minor version 7.0.0.BETA2

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -18,7 +18,7 @@
  * DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
  * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
  */
-module data.api
+module io.github.astrapisixtynine.data.api
 {
 	exports io.github.astrapi69.data.accomplishable;
 	exports io.github.astrapi69.data.activatable;
